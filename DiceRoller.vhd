1c24ad6701d87150cbf825feb70929b74228c0da
Rhyzan-Git <77850528+Rhyzan-Git@users.noreply.github.com>
2021-05-20 15:06:21 -0400

    Update DiceRoller.vhd

--- DiceRoller.vhd
+++ DiceRoller.vhd
@@ -20,8 +20,8 @@ use IEEE.std_logic_unsigned.all;
 
 entity LFSRDiceRoller is
 --Ports + 12Mhz system clock 
-port (	sysClk : in std:logic; 					--12Mhz System Clock
-	Reset : inout std_ logic; 				--Used for 7-seg display driver logic
+port (	sysClk : in std_logic; 					--12Mhz System Clock
+	Reset : inout std_logic; 				--Used for 7-seg display driver logic
 	Roll_Button,Select_Button,Clear_Button : in std_logic;	--User input buttons
       	Display_7seg_LED : out std_logic_vector (7 downto 0);	--For each 7-seg display LED
 	Enable_7seg : inout std_logic_vector(3 downto 0)	--Shift Register to enable 7-Seg Displays
@@ -30,9 +30,9 @@ port (	sysClk : in std:logic; 					--12Mhz System Clock
 --Signals
 
 --LFSR clock pre-scaler
-signal LFSR_clk_prescaler : std_logic_vector (11 downto 0) := “101110111000”;
+signal LFSR_clk_prescaler : std_logic_vector (11 downto 0) := "101110111000";
 signal LFSR_clk_prescaler_counter : std_logic_vector (11 downto 0) := (others => '0');
-signal LFSR_clk : std_logic := ‘0’;
+signal LFSR_clk : std_logic := '0';
 
 --For LFSR Logic
 signal LFSR_output: std_logic_vector (7 DOWNTO 0);		--LFSR output signal (8-bits)		
@@ -40,9 +40,9 @@ signal LFSR_current_state, LFSR_next_state: std_logic_vector (7 DOWNTO 0);	--LFS
 signal LFSR_feedback: std_logic;				--LFSR XOR Feedback loop
 	
 --Debounce clock pre-scaler
-signal Debounce_clk_prescaler : std_logic_vector (15 downto 0) := “1110101001100000”;
+signal Debounce_clk_prescaler : std_logic_vector (15 downto 0) := "1110101001100000";
 signal Debounce_clk_prescaler_counter : std_logic_vector (15 downto 0) := (others => '0');
-signal Debounce_clk : std_logic := ‘0’;
+signal Debounce_clk : std_logic := '0';
 	
 --For Debounce Logic
 signal Roll_button_debounced : std_logic;	--Single pulse for Roll button
@@ -53,9 +53,9 @@ signal SB_debounce_1, SB_debounce_2, SB_debounce_3 : std_logic;	--Shift Register
 signal CB_debounce_1, CB_debounce_2, CB_debounce_3 : std_logic;	--Shift Registers for Clear debounce
 
 --7-Seg Display clock pre-scaler
-signal Display_clk_prescaler : std_logic_vector (13 downto 0) := “11101010011000”;
+signal Display_clk_prescaler : std_logic_vector (13 downto 0) := "11101010011000";
 signal Dispaly_clk_prescaler_counter : std_logic_vector (13 downto 0) := (others => '0');
-signal Display_clk : std_logic := ‘0’;
+signal Display_clk : std_logic := '0';
 
 --For Dice Selection Logic
 signal Selected_dice_current, Selected_dice_next : unsigned(2 downto 0);
@@ -66,10 +66,10 @@ signal d4_filter_output, d6_filter_output : unsigned (7 downto 0) := (others => 
 signal d8_filter_output, d10_filter_output : unsigned (7 downto 0) := (others => '0'); 
 signal d12_filter_output, d20_filter_output  : unsigned (7 downto 0) := (others => '0');				
 signal d100_filter_output, LFSR_output_in : unsigned (7 downto 0) := (others => '0');
-signal d4_number_pool, d6_number_pool : std_logic_vector (7 downto 0) : (others => '0');
-signal d8_number_pool, d10_number_pool : std_logic_vector (7 downto 0) : (others => '0');
-signal d12_number_pool, d20_number_pool : std_logic_vector (7 downto 0) : (others => '0');
-signal d100_number_pool : std_logic_vector (7 downto 0) : (others => '0');
+signal d4_number_pool, d6_number_pool : std_logic_vector (7 downto 0) := (others => '0');
+signal d8_number_pool, d10_number_pool : std_logic_vector (7 downto 0) := (others => '0');
+signal d12_number_pool, d20_number_pool : std_logic_vector (7 downto 0) := (others => '0');
+signal d100_number_pool : std_logic_vector (7 downto 0) := (others => '0');
 
 --For Random Number Pool logic
 signal Number_pool_output : std_logic_vector(7 downto 0);
@@ -78,9 +78,11 @@ end LFSRDiceRoller;
 
 architecture LFSRDiceRoller_behavioral of LFSRDiceRoller is
 
+begin	   
+
 process (sysClk)
 
-begin	   
+begin
 ---------------------------------------------------------------------------------------------
 --LFSR clock
 --Generates a 2khz clock from the 12Mhz system clock
@@ -89,7 +91,7 @@ if rising_edge(sysClk) then
 	LFSR_clk_prescaler_counter <= LFSR_clk_prescaler_counter + 1;
 	if (LFSR_clk_prescaler_counter > LFSR_clk_prescaler) then 
 		LFSR_clk <= not LFSR_clk;
-	  	LFSR_clk_prescaler_counter <= (others => ‘0’);
+	  	LFSR_clk_prescaler_counter <= (others => '0');
 	end if;
 end if;	
 ---------------------------------------------------------------------------------------------
@@ -121,7 +123,7 @@ if rising_edge(sysClk) then
 	Debounce_clk_prescaler_counter <= Debounce_clk_prescaler_counter + 1;
 	if (Debounce_clk_prescaler_counter > Debounce_clk_prescaler) then 
 		Debounce_clk <= not Debounce_clk;
-	  	Debounce_clk_prescaler_counter <= (others => ‘0’);
+	  	Debounce_clk_prescaler_counter <= (others => '0');
 	end if;
 end if;	
 ---------------------------------------------------------------------------------------------	
@@ -132,13 +134,13 @@ if rising_edge(sysClk) then
 	Display_clk_prescaler_counter <= Display_clk_prescaler_counter + 1;
 	if (Display_clk_prescaler_counter > Display_clk_prescaler) then 
 		Display_clk <= not Display_clk;
-	  	Display_clk_prescaler_counter <= (others => ‘0’);
+	  	Display_clk_prescaler_counter <= (others => '0');
 	end if;
 end if;
 	
 --Enables the 7-seg Displays
-if (reset = ‘1’) then
-	Enable_7seg <= “0001”;
+if (reset = '1') then
+	Enable_7seg <= "0001";
 elsif rising_edge(Display_clk) then
 	Enable_7seg(1) <= Enable_7seg(0); Enable_7seg(2) <= Enable_7seg(1); 
 	Enable_7seg(3) <= Enable_7seg(2); Enable_7seg(0) <= Enable_7seg(3);	
@@ -172,7 +174,7 @@ Clear_button_debounced <= CB_debounce_1 and CB_debounce_2 and not CB_debounce_3;
 --	Interects with Filter for Valid Numbers to change parameters
 
 if (Reset = '1') then
-	Selected_dice_current <= “000”;
+	Selected_dice_current <= "000";
 elsif rising_edge(Select_button_debounced) then
 	Selected_dice_current <= Selected_dice_next;
 end if;
@@ -194,7 +196,7 @@ elsif rising_edge(LFSR_clk) then
 end if;
 
 --Valid d4 numbers
---if Selected_dice_output = “000” then
+--if Selected_dice_output = "000" then
 d4_filter_output <= d4_filter_output when 
 	       (LFSR_output_in(2 downto 0)>=5 or LFSR_output_in(2 downto 0)=0) else	--Checks if first 3 bits of LFSR output is between 1 and 4
 	LFSR_output_in(7 downto 0);							--Saves LFSR number to filter (Only first 3 bits are important)
@@ -204,7 +206,7 @@ d4_number_pool <= std_logic_vector(d4_filter_output);					--assigns filtered num
 --end if;
 
 --Valid d6 numbers
---elsif Selected_dice_output = “001”
+--elsif Selected_dice_output = "001"
 d6_filter_output <= d6_filter_output when 
 	       (LFSR_output_in(2 downto 0)=7 or LFSR_output_in(2 downto 0)=0) else	--Checks if first 3 bits of LFSR output is between 1 and 6
 	LFSR_output_in(7 downto 0);							--Saves LFSR number to filter (Only first 3 bits are important)
@@ -214,7 +216,7 @@ d6_number_pool <= std_logic_vector(d6_filter_output);					--assigns filtered num
 --end if;
 
 --Valid d8 numbers
---elsif Selected_dice_output = “010”
+--elsif Selected_dice_output = "010"
 d8_filter_output <= d8_filter_output when 
 	       (LFSR_output_in(3 downto 0)>=9 or LFSR_output_in(3 downto 0)=0) else	--Checks if first 4 bits of LFSR output is between 1 and 8
 	LFSR_output_in(7 downto 0);							--Saves LFSR number to filter (Only first 4 bits are important)
@@ -224,7 +226,7 @@ d8_number_pool <= std_logic_vector(d8_filter_output);					--assigns filtered num
 --end if;
 
 --Valid d10 numbers
---elsif Selected_dice_output = “011”
+--elsif Selected_dice_output = "011"
 d10_filter_output <= d10_filter_output when 
 	       (LFSR_output_in(3 downto 0)>=11 or LFSR_output_in(3 downto 0)=0) else	--Checks if first 4 bits of LFSR output is between 1 and 10
 	LFSR_output_in(7 downto 0);							--Saves LFSR number to filter (Only first 4 bits are important)
@@ -234,7 +236,7 @@ d10_number_pool <= std_logic_vector(d10_filter_output);					--assigns filtered n
 --end if;
 
 --Valid d12 numbers
---elsif Selected_dice_output = “100”
+--elsif Selected_dice_output = "100"
 d12_filter_output <= d12_filter_output when 
 	       (LFSR_output_in(3 downto 0)>=13 or LFSR_output_in(3 downto 0)=0) else	--Checks if first 4 bits of LFSR output is between 1 and 12
 	LFSR_output_in(7 downto 0);							--Saves LFSR number to filter (Only first 4 bits are important)
@@ -244,7 +246,7 @@ d12_number_pool <= std_logic_vector(d12_filter_output);					--assigns filtered n
 --end if;
 
 --Valid d20 numbers
---elsif Selected_dice_output = “101”
+--elsif Selected_dice_output = "101"
 d20_filter_output <= d20_filter_output when 
 	       (LFSR_output_in(4 downto 0)>=21 or LFSR_output_in(4 downto 0)=0) else	--Checks if first 4 bits of LFSR output is between 1 and 10
 	LFSR_output_in(7 downto 0);							--Saves LFSR number to filter (Only first 4 bits are important)
@@ -254,7 +256,7 @@ d20_number_pool <= std_logic_vector(d20_filter_output);					--assigns filtered n
 --end if;
 
 --Valid d100 numbers
---elsif Selected_dice_output = “110”
+--elsif Selected_dice_output = "110"
 d100_filter_output <= d100_filter_output when 
 	       (LFSR_output_in(6 downto 0)>=101 or LFSR_output_in(6 downto 0)=0) else	--Checks if first 4 bits of LFSR output is between 1 and 10
 	LFSR_output_in(7 downto 0);							--Saves LFSR number to filter (Only first 4 bits are important)
@@ -266,14 +268,14 @@ d100_number_pool <= std_logic_vector(d100_filter_output);				--assigns filtered 
 --Random Number Pool
 --	Stores strings of bits based on what is currently inside the Filter of Valid Numbers
 if rising_edge(Roll_button_debounced) then
-	   if Selected_dice_output = “000” then Number_pool_output <= d4_number_pool;
-	elsif Selected_dice_output = “001” then Number_pool_output <= d6_number_pool;
-	elsif Selected_dice_output = “010” then Number_pool_output <= d8_number_pool;
-	elsif Selected_dice_output = “111” then Number_pool_output <= d10_number_pool;
-	elsif Selected_dice_output = “101” then Number_pool_output <= d12_number_pool;
-	elsif Selected_dice_output = “101” then Number_pool_output <= d20_number_pool;
-	elsif Selected_dice_output = “110” then Number_pool_output <= d100_number_pool;
-	elsif Selected_dice_output = “111” then Number_pool_output <= "00000000";
+	   if Selected_dice_output = "000" then Number_pool_output <= d4_number_pool;
+	elsif Selected_dice_output = "001" then Number_pool_output <= d6_number_pool;
+	elsif Selected_dice_output = "010" then Number_pool_output <= d8_number_pool;
+	elsif Selected_dice_output = "111" then Number_pool_output <= d10_number_pool;
+	elsif Selected_dice_output = "101" then Number_pool_output <= d12_number_pool;
+	elsif Selected_dice_output = "101" then Number_pool_output <= d20_number_pool;
+	elsif Selected_dice_output = "110" then Number_pool_output <= d100_number_pool;
+	elsif Selected_dice_output = "111" then Number_pool_output <= "00000000";
 	end if;
 end if;
 ---------------------------------------------------------------------------------------------
@@ -322,25 +324,25 @@ end process ;
 --7-Seg Display logic (Selected Dice)
 --	Used to display currently selected dice
 if (Enable_7seg = "1000") then 						--Displays 10s place for Selected dice
-	   if Selected_dice_output = “000” then Display_7seg_LED <= "0000000";	--d4	--Displays Blank
-	elsif Selected_dice_output = “001” then Display_7seg_LED <= "0000000";	--d6	--Displays Blank
-	elsif Selected_dice_output = “010” then Display_7seg_LED <= "0000000"; 	--d8	--Displays Blank
-	elsif Selected_dice_output = “011” then Display_7seg_LED <= "0000110"; 	--d10	--Displays 1
-	elsif Selected_dice_output = “100” then Display_7seg_LED <= "0000110"; 	--d12	--Displays 1
-	elsif Selected_dice_output = “101” then Display_7seg_LED <= "1011011"; 	--d20	--Displays 2
-	elsif Selected_dice_output = “110” then Display_7seg_LED <= "0111111"; 	--d1(00)--Displays 0
-	elsif Selected_dice_output = “111” then Display_7seg_LED <= "0000000"; 	--Blank (Not used)
+	   if Selected_dice_output = "000" then Display_7seg_LED <= "0000000";	--d4	--Displays Blank
+	elsif Selected_dice_output = "001" then Display_7seg_LED <= "0000000";	--d6	--Displays Blank
+	elsif Selected_dice_output = "010" then Display_7seg_LED <= "0000000"; 	--d8	--Displays Blank
+	elsif Selected_dice_output = "011" then Display_7seg_LED <= "0000110"; 	--d10	--Displays 1
+	elsif Selected_dice_output = "100" then Display_7seg_LED <= "0000110"; 	--d12	--Displays 1
+	elsif Selected_dice_output = "101" then Display_7seg_LED <= "1011011"; 	--d20	--Displays 2
+	elsif Selected_dice_output = "110" then Display_7seg_LED <= "0111111"; 	--d1(00)--Displays 0
+	elsif Selected_dice_output = "111" then Display_7seg_LED <= "0000000"; 	--Blank (Not used)
 	end if;
 	       
 elsif (Enable_7seg = "0100") then 						--Displays 1s place for Selected Dice
-	   if Selected_dice_output = “000” then Display_7seg_LED <= "1100110";	--d4	--Displays 4
-	elsif Selected_dice_output = “001” then Display_7seg_LED <= "1111101";	--d6	--Displays 6
-	elsif Selected_dice_output = “010” then Display_7seg_LED <= "1111111"; 	--d8	--Displays 8
-	elsif Selected_dice_output = “011” then Display_7seg_LED <= "0111111"; 	--d10	--Displays 0
-	elsif Selected_dice_output = “100” then Display_7seg_LED <= "1011011"; 	--d12	--Displays 2
-	elsif Selected_dice_output = “101” then Display_7seg_LED <= "0111111"; 	--d20	--Displays 0
-	elsif Selected_dice_output = “110” then Display_7seg_LED <= "0111111"; 	--d1(00)--Displays 0
-	elsif Selected_dice_output = “111” then Display_7seg_LED <= "0000000"; 	--Blank (Not used)
+	   if Selected_dice_output = "000" then Display_7seg_LED <= "1100110";	--d4	--Displays 4
+	elsif Selected_dice_output = "001" then Display_7seg_LED <= "1111101";	--d6	--Displays 6
+	elsif Selected_dice_output = "010" then Display_7seg_LED <= "1111111"; 	--d8	--Displays 8
+	elsif Selected_dice_output = "011" then Display_7seg_LED <= "0111111"; 	--d10	--Displays 0
+	elsif Selected_dice_output = "100" then Display_7seg_LED <= "1011011"; 	--d12	--Displays 2
+	elsif Selected_dice_output = "101" then Display_7seg_LED <= "0111111"; 	--d20	--Displays 0
+	elsif Selected_dice_output = "110" then Display_7seg_LED <= "0111111"; 	--d1(00)--Displays 0
+	elsif Selected_dice_output = "111" then Display_7seg_LED <= "0000000"; 	--Blank (Not used)
 	end if;
 ---------------------------------------------------------------------------------------------
 --7-Seg Display logic (Rolled Dice)
