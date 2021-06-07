2c80e0d43e2a89d7c4f1ef2889396c3f71a7d9e7
Rhyzan-Git <77850528+Rhyzan-Git@users.noreply.github.com>
2021-06-07 10:45:20 -0400

    Update WatchDogTimer.v

--- WatchDogTimer.v
+++ WatchDogTimer.v
@@ -7,44 +7,48 @@ restart must trigger on rising edge of pulse
 
 module watchdogTimer
   (
-    clk_1khz,  //assumes a 1khz clock running inside the system
+    clk_1khz,  //1khz clock running inside the system
+    wd_in,     //Input signal from motor
+    wd_out     //Output signal to trigger shutdown
     );
 
-input clk_1khz;   //1khz
+input clk_1khz;
 input wd_in;
 output wd_out;
   
 //Constants
-parameter c_clk_8hz = 750000; // uses 12Mhz clock for shutdown signal
+//uses 1khz signal as a counter
+//hitting this limit triggers the shutdown signal (wd_out)
+parameter c_shtdwn = 63; 
 
 //Counters
-reg [19:0] r_clk_8hz = 0;
-
-//Signal toggles
-reg t_clk_8hz = 1'b0;
-
+reg [5:0] r_shtdwn_cnt = 0;
+  
+//Wires  
+wire edge_detect; //Edge detector of watchdog in
+wire wd_in_Q;     //Q side of wd_in register
+  
 begin
  
-  //Generates 8hz timer, runs off os 12Mhz system clock
-  always @ (posedge const_clk)
-    begin
-
-      //restart condition
-      if (posedge ck_1khz)  
-        begin        
-          t_clk_8hz <= '0';
-          r_clk_8hz <= 0;
-        end
-
-      //No restart detected
-      else
-        r_clk_8hz <= r_clk_8hz + 1;
+//Edge detector
+  always @ (posedge clk_1khz)
+    begin  
+      wd_in_Q <= wd_in     
     end
+  edge_detect <= wd_in xor wd_in_Q;   
 
-  //watchdog triggers
-  if (r_clk_8hz == c_clk_8hz)
+//Shutdown timer
+  always @ (posedge clk_1khz)
     begin
-      shutdown <= '1';
+      //Triggers shutdown ~126ms after last detected motor signal
+      if (r_shtdwn_cnt == c_shtdwn-1) // -1, since counter starts at 0   
+          wd_out <= '1';
+      else if (edge_detect == '1') begin
+          wd_out <= '0';
+          r_shtdwn_cnt <= 0;
+      end
+      else
+          r_shtdwn_cnt <= r_shtdwn_cnt + 1;
     end
   
 end module
