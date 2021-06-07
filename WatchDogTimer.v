23cdc07eb234f221bf1f67b0ca251d74e75fdde6
Rhyzan-Git <77850528+Rhyzan-Git@users.noreply.github.com>
2021-06-07 12:03:13 -0400

    Update WatchDogTimer.v

--- WatchDogTimer.v
+++ WatchDogTimer.v
@@ -1,3 +1,23 @@
+//`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/07/2021 10:50:10 AM
+// Design Name: 
+// Module Name: WatchDogTimer
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 /* Notes: 
 Written in Verilog
 Shutdown triggers 1/8th of a second after not seeing restart pulse
@@ -5,16 +25,12 @@ working with 1khz clock
 restart must trigger on rising edge of pulse
 */
 
-module watchdogTimer
+module WatchDogTimer
   (
-    clk_1khz,  //1khz clock running inside the system
-    wd_in,     //Input signal from motor
-    wd_out     //Output signal to trigger shutdown
+    input clk_1khz,  //1khz clock running inside the system
+    input wd_in,     //Input signal from motor
+    output wd_out     //Output signal to trigger shutdown
     );
-
-input clk_1khz;
-input wd_in;
-output wd_out;
   
 //Constants
 //uses 1khz signal as a counter
@@ -26,29 +42,33 @@ reg [5:0] r_shtdwn_cnt = 0;
   
 //Wires  
 wire edge_detect; //Edge detector of watchdog in
-wire wd_in_Q;     //Q side of wd_in register
-  
+reg wd_shtdwn;    //Shutdown signal to be assigned to output
+reg wd_in_Q;      //Q side of wd_in register
+
 begin
  
 //Edge detector
-  always @ (posedge clk_1khz)
-    begin  
-      wd_in_Q <= wd_in     
-    end
-  edge_detect <= wd_in xor wd_in_Q;   
+  always @ (posedge clk_1khz) begin
+        wd_in_Q <= wd_in;
+  end     
+//assigns edge detect to wd_in xor (^) wd_in_q     
+assign  edge_detect = wd_in ^ wd_in_Q;   
 
 //Shutdown timer
   always @ (posedge clk_1khz)
-    begin
+  begin
       //Triggers shutdown ~126ms after last detected motor signal
       if (r_shtdwn_cnt == c_shtdwn-1) // -1, since counter starts at 0   
-          wd_out <= '1';
-      else if (edge_detect == '1') begin
-          wd_out <= '0';
-          r_shtdwn_cnt <= 0;
-      end
-      else
-          r_shtdwn_cnt <= r_shtdwn_cnt + 1;
-    end
-  
-end module
+            wd_shtdwn <= "1"; 
+      else begin   
+          if (edge_detect == "1") begin
+                wd_shtdwn <= "0";
+                r_shtdwn_cnt <= 0;
+          end else
+          r_shtdwn_cnt <= r_shtdwn_cnt + 1;      
+      end   
+   end   
+assign wd_out = wd_shtdwn;   
+
+end 
+endmodule
